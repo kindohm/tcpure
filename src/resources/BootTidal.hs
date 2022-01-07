5b55429cd0bc32b5349dec365cc9fb9886c35aea
Mike Hodnick <mike@kindohm.com>
2022-01-07 15:55:22 -0600

    add stuff to BootTidal.hs

--- src/resources/BootTidal.hs
+++ src/resources/BootTidal.hs
@@ -13,8 +13,6 @@ tidal <- startTidal (superdirtTarget {oLatency = 0.1, oAddress = "127.0.0.1", oP
 let only = (hush >>)
     p = streamReplace tidal
     hush = streamHush tidal
-    panic = do hush
-               once $ sound "superpanic"
     list = streamList tidal
     mute = streamMute tidal
     unmute = streamUnmute tidal
@@ -31,23 +29,6 @@ let only = (hush >>)
     setcps = asap . cps
     getcps = streamGetcps tidal
     getnow = streamGetnow tidal
-    xfade i = transition tidal True (Sound.Tidal.Transition.xfadeIn 4) i
-    xfadeIn i t = transition tidal True (Sound.Tidal.Transition.xfadeIn t) i
-    histpan i t = transition tidal True (Sound.Tidal.Transition.histpan t) i
-    wait i t = transition tidal True (Sound.Tidal.Transition.wait t) i
-    waitT i f t = transition tidal True (Sound.Tidal.Transition.waitT f t) i
-    jump i = transition tidal True (Sound.Tidal.Transition.jump) i
-    jumpIn i t = transition tidal True (Sound.Tidal.Transition.jumpIn t) i
-    jumpIn' i t = transition tidal True (Sound.Tidal.Transition.jumpIn' t) i
-    jumpMod i t = transition tidal True (Sound.Tidal.Transition.jumpMod t) i
-    mortal i lifespan release = transition tidal True (Sound.Tidal.Transition.mortal lifespan release) i
-    interpolate i = transition tidal True (Sound.Tidal.Transition.interpolate) i
-    interpolateIn i t = transition tidal True (Sound.Tidal.Transition.interpolateIn t) i
-    clutch i = transition tidal True (Sound.Tidal.Transition.clutch) i
-    clutchIn i t = transition tidal True (Sound.Tidal.Transition.clutchIn t) i
-    anticipate i = transition tidal True (Sound.Tidal.Transition.anticipate) i
-    anticipateIn i t = transition tidal True (Sound.Tidal.Transition.anticipateIn t) i
-    forId i t = transition tidal False (Sound.Tidal.Transition.mortalOverlay t) i
     d1 = p 1 . (|< orbit 0)
     d2 = p 2 . (|< orbit 1)
     d3 = p 3 . (|< orbit 2)
@@ -60,10 +41,6 @@ let only = (hush >>)
     d10 = p 10 . (|< orbit 9)
     d11 = p 11 . (|< orbit 10)
     d12 = p 12 . (|< orbit 11)
-    d13 = p 13
-    d14 = p 14
-    d15 = p 15
-    d16 = p 16
 :}
 
 :{
