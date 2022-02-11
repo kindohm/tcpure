35e18adc88e29a781611ae9829f55e38662ee736
Mike Hodnick <mike@kindohm.com>
2022-02-10 19:33:07 -0600

    do not use big blocks

--- src/resources/BootTidal.hs
+++ src/resources/BootTidal.hs
@@ -9,48 +9,44 @@ hSetEncoding stdout utf8
 -- total latency = oLatency + cFrameTimespan
 tidal <- startTidal (superdirtTarget {oLatency = 0.1, oAddress = "127.0.0.1", oPort = 57120}) (defaultConfig {cVerbose = True, cFrameTimespan = 1/20})
 
-:{
 let only = (hush >>)
-    p = streamReplace tidal
-    hush = streamHush tidal
-    list = streamList tidal
-    mute = streamMute tidal
-    unmute = streamUnmute tidal
-    unmuteAll = streamUnmuteAll tidal
-    unsoloAll = streamUnsoloAll tidal
-    solo = streamSolo tidal
-    unsolo = streamUnsolo tidal
-    once = streamOnce tidal
-    first = streamFirst tidal
-    asap = once
-    nudgeAll = streamNudgeAll tidal
-    all = streamAll tidal
-    resetCycles = streamResetCycles tidal
-    setcps = asap . cps
-    getcps = streamGetcps tidal
-    getnow = streamGetnow tidal
-    d1 = p 1 . (|< orbit 0)
-    d2 = p 2 . (|< orbit 1)
-    d3 = p 3 . (|< orbit 2)
-    d4 = p 4 . (|< orbit 3)
-    d5 = p 5 . (|< orbit 4)
-    d6 = p 6 . (|< orbit 5)
-    d7 = p 7 . (|< orbit 6)
-    d8 = p 8 . (|< orbit 7)
-    d9 = p 9 . (|< orbit 8)
-    d10 = p 10 . (|< orbit 9)
-    d11 = p 11 . (|< orbit 10)
-    d12 = p 12 . (|< orbit 11)
-:}
+let p = streamReplace tidal
+let hush = streamHush tidal
+let list = streamList tidal
+let mute = streamMute tidal
+let unmute = streamUnmute tidal
+let unmuteAll = streamUnmuteAll tidal
+let unsoloAll = streamUnsoloAll tidal
+let solo = streamSolo tidal
+let unsolo = streamUnsolo tidal
+let once = streamOnce tidal
+let first = streamFirst tidal
+let asap = once
+let nudgeAll = streamNudgeAll tidal
+let all = streamAll tidal
+let resetCycles = streamResetCycles tidal
+let setcps = asap . cps
+let getcps = streamGetcps tidal
+let getnow = streamGetnow tidal
+let d1 = p 1 . (|< orbit 0)
+let d2 = p 2 . (|< orbit 1)
+let d3 = p 3 . (|< orbit 2)
+let d4 = p 4 . (|< orbit 3)
+let d5 = p 5 . (|< orbit 4)
+let d6 = p 6 . (|< orbit 5)
+let d7 = p 7 . (|< orbit 6)
+let d8 = p 8 . (|< orbit 7)
+let d9 = p 9 . (|< orbit 8)
+let d10 = p 10 . (|< orbit 9)
+let d11 = p 11 . (|< orbit 10)
+let d12 = p 12 . (|< orbit 11)
 
-:{
 let getState = streamGet tidal
-    setI = streamSetI tidal
-    setF = streamSetF tidal
-    setS = streamSetS tidal
-    setR = streamSetR tidal
-    setB = streamSetB tidal
-:}
+let setI = streamSetI tidal
+let setF = streamSetF tidal
+let setS = streamSetS tidal
+let setR = streamSetR tidal
+let setB = streamSetB tidal
 
 :set prompt "tidal> "
 :set prompt-cont ""
